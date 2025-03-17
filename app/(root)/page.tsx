1a8063a8bda649337ce09447f22f03d329595244
SomisCreations <89362410+samriddhighosh@users.noreply.github.com>
2025-03-16 19:14:02 -0500

    initial-commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-center'>
+      <p>Home</p>
+
+    </div>
+  )
+}
+
+export default Home
---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,18 @@
+// clerkId, email, username, photo, firstName, lastName, planId, creditBalance
+
+import { model, models, Schema } from "mongoose";
+
+const UserSchema = new Schema({
+    clerId: {type:Number, required:true, unique:true},
+    email: {type:String, required:true, unique:true},
+    username: {type:String, required: true, unique:true},
+    photo:{type:String, required:true},
+    firstName:{type:String},
+    lastName:{type:String},
+    planId:{type:Number, default:1},
+    creditBalance: {type:Number, default:10}
+})
+
+const User = models?.User || model("User", UserSchema)
+
+export default User;
