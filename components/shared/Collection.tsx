e1ca7ce7da21625c83af1363df68bba996998977
SomisCreations <89362410+samriddhighosh@users.noreply.github.com>
2025-12-28 13:28:42 -0600

    commit

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -56,7 +56,7 @@ export const Collection = ({
       {images.length > 0 ? (
         <ul className="collection-list">
           {images.map((image) => (
-            <Card image={image} key={image._id} />
+            <Card image={image} key={image?.author?._id} />
           ))}
         </ul>
       ) : (
@@ -97,7 +97,7 @@ export const Collection = ({
 const Card = ({ image }: { image: IImage }) => {
   return (
     <li>
-      <Link href={`/transformations/${image._id}`} className="collection-card">
+      <Link href={`/transformations/${image?.author?._id}`} className="collection-card">
         <CldImage
           src={image.publicId}
           alt={image.title}
